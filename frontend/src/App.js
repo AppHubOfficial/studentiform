5f64a87ee770692c2e3bad46dd875aa9fafbfb64
simone <simone07andreotti@gmail.com>
2024-10-08 09:05:44 +0200

    Initialize

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <Page404 />,
+    children: [
+      {
+        path: "/",
+        element: <Home />, 
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/chatroom",
+        element: <ChatRoom />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
