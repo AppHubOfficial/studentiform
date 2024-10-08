c4ff55115f29143ba50c8e89dbc2974a92d097b2
simone <simone07andreotti@gmail.com>
2024-10-08 09:31:45 +0200

    Initialize2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import '../assets/styles/Home.css';
 
 function Home() {
-  return <h1>Welcome to the Home Page</h1>;
+  return <h1 className='title'>Welcome to the Home Page</h1>;
 }
 
 export default Home;
