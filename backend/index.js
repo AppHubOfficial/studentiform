4f8571193582e6356fb8a02579ebf706256e9c70
simone <simone07andreotti@gmail.com>
2024-12-06 16:20:56 +0100

    Title

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const Redis = require('ioredis');
 const userRoutes = require('./routes/userRoutes');
+
+require('dotenv').config();
 
 const app = express();
 
+// console.log(process.env.FRONTEND_URL)
+
+const redisClient = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD,
+});
+
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: `${process.env.FRONTEND_URL}`,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 };
@@ -14,6 +26,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(session({
+  store: new RedisStore({ client: redisClient }),
   secret: '89g^_oPt7_0GYlC;YV#[8&uQ&3b,v&!q',
   resave: false,
   saveUninitialized: false,
