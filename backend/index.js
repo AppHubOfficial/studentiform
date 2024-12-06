909f344b67112f8a7cc110814274a7d544e39d49
simone <simone07andreotti@gmail.com>
2024-12-06 16:22:38 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
-const RedisStore = require('connect-redis')(session);
-const Redis = require('ioredis');
 const userRoutes = require('./routes/userRoutes');
+
 
 require('dotenv').config();
 
 const app = express();
 
 // console.log(process.env.FRONTEND_URL)
-
-const redisClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASSWORD,
-});
 
 const corsOptions = {
   origin: `${process.env.FRONTEND_URL}`,
@@ -26,7 +19,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(session({
-  store: new RedisStore({ client: redisClient }),
   secret: '89g^_oPt7_0GYlC;YV#[8&uQ&3b,v&!q',
   resave: false,
   saveUninitialized: false,
